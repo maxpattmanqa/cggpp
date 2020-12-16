0388e6442a29fe964442f651b35344b93d7b6112
maxpa_qzrio <mpattman@qa.com>
2020-12-16 12:18:37 +0000

    update sus 3

--- startup.sh
+++ startup.sh
@@ -1,19 +1,31 @@
-#!/bin/bash
-echo $WORKSPACE
-echo $GIT_CHECKOUT_DIR
+# #!/bin/bash
+# echo $WORKSPACE
+# echo $GIT_CHECKOUT_DIR
 
-sudo apt-get update
-sudo apt-get install -y python3 
-sudo apt-get install -y python3-venv 
-sudo apt-get install -y python3-pip
-cd $WORKSPACE
-ls -lah
-sudo cp -r $WORKSPACE /opt/jenkins
-sudo chown -R jenkins /opt/jenkins
+# sudo apt-get update
+# sudo apt-get install -y python3 
+# sudo apt-get install -y python3-venv 
+# sudo apt-get install -y python3-pip
+# cd $WORKSPACE
+# ls -lah
+# sudo cp -r $WORKSPACE /opt/jenkins
+# sudo chown -R jenkins /opt/jenkins
 
-cd /opt/jenkins
+# cd /opt/jenkins
+# python3 -m venv venv
+# source venv/bin/activate
+# pip3 install -r requirements.txt
+ 
+# sudo systemctl restart cggpp
+
+#!/bin/bash 
+sudo apt update
+sudo apt-get install python3-venv
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
- 
-sudo systemctl restart cggpp
+sudo mkdir /opt/cggpp
+sudo chown -R jenkins /opt/cggpp
+sudo systemctl daemon-reload
+sudo systemctl stop app.service
+sudo systemctl start app.service
