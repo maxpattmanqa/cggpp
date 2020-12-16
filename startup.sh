573a696b60978b6e027d0fe92d791a6ec1ca62dc
maxpattmanqa <mpattman@qa.com>
2020-12-16 10:33:30 +0000

    startup.sh added to dir

---
+++ startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo apt-get install -y python3-pip
+cd $WORKSPACE
+ls -lah
+sudo cp -r $WORKSPACE /opt/jenkins
+sudo chown -R jenkins /opt/jenkins
+
+cd /opt/jenkins
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+
+sudo systemctl daemon-reload
+sudo systemctl stop cggpp.service
+sudo systemctl start cggpp.service
