704db116e8416bdb8c1ab9799159b7ec7d2f9773
maxpa_qzrio <mpattman@qa.com>
2020-12-16 19:56:37 +0000

    this

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import getenv
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
+Bootstrap(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URI")
 
 app.config["SECRET_KEY"] = getenv("SECRET_KEY")
