c1c5d7d3b6c95de1060b5525886774462aa79db0
maxpa_qzrio <mpattman@qa.com>
2020-12-16 11:02:54 +0000

    update sus 2

--- startup.sh
+++ startup.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
+echo $WORKSPACE
+echo $GIT_CHECKOUT_DIR
+
+sudo apt-get update
+sudo apt-get install -y python3 
+sudo apt-get install -y python3-venv 
 sudo apt-get install -y python3-pip
 cd $WORKSPACE
 ls -lah
 sudo cp -r $WORKSPACE /opt/jenkins
 sudo chown -R jenkins /opt/jenkins
 
-cd /opt/jenkins 
+cd /opt/jenkins
 python3 -m venv venv
-ls -lah
-cd ..
 source venv/bin/activate
 pip3 install -r requirements.txt
-
-sudo systemctl daemon-reload
-sudo systemctl stop cggpp.service
-sudo systemctl start cggpp.service
+ 
+sudo systemctl restart cggpp
