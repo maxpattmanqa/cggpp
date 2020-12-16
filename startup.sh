6d91edd0af2cc08ca2a05dac5a47051de491e438
maxpa_qzrio <mpattman@qa.com>
2020-12-16 11:01:27 +0000

    update sus

--- startup.sh
+++ startup.sh
@@ -5,9 +5,10 @@ ls -lah
 sudo cp -r $WORKSPACE /opt/jenkins
 sudo chown -R jenkins /opt/jenkins
 
-cd /opt/jenkins
+cd /opt/jenkins 
 python3 -m venv venv
 ls -lah
+cd ..
 source venv/bin/activate
 pip3 install -r requirements.txt
 
